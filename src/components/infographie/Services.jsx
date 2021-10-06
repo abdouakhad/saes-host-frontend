2860d753d030badceab7469fe0ce55d9c7bfe178
Abdoul Akhad Top <akhad0015@gmail.com>
2021-10-06 22:04:20 +0000

    almost complete french saes

--- src/components/infographie/Services.jsx
+++ src/components/infographie/Services.jsx
@@ -1,41 +1,38 @@
-import React from 'react';
+import React from 'react'
 import SingleService from './SingleService'
 import service0 from '../../svg/11-Delivery-rafiki.svg'
 import service1 from '../../svg/12-pencil-alt-solid.svg'
 import service2 from '../../svg/13-rocket-solid.svg'
 import service3 from '../../svg/14-money-check-alt-solid.svg'
 
-const imgs = [service0, service1, service2, service3]
+const p1 =
+  'Lorem ipsumLorem ipsum dolor sit amet, Infography elit, sed do eiusmod Logo incididunt ut la.'
 
-const p1 = 'Lorem ipsumLorem ipsum dolor sit amet, Infography elit, sed do eiusmod Logo incididunt ut la.'
-
-function Services(){
-    return (
-        <div className="page3 my-5">
-            <div class="bg-black text-white py-8 text-center">
-                <h1 class='text-3xl font-bold special'>
-                    Our Services
-                </h1>
-            </div>
-            <div class="service1 flex flex-col text-black justify-center items-center">
-                <div class="svg w-96">
-                    <img src={service0} alt = 'Service' />
-                </div>
-                <p class=' w-8/12 m-auto text-center font-semi-bold tracking-wider'>
-                    At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
-                    deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non
-                    provident.
-                </p>
-            </div>
-            <div class="info w-9/12 mt-8 text-black m-auto">
-                <div class="grid md:grid-cols-3 grid-cols-1 gap-3 justify-center items-center">
-                    <SingleService img = {service1} text = {p1} />
-                    <SingleService img = {service2} text = {p1} />
-                    <SingleService img = {service3} text = {p1} />
-                </div>
-            </div>
+function Services() {
+  return (
+    <div className='page3 my-5'>
+      <div class='bg-black text-white py-8 text-center'>
+        <h1 class='text-3xl font-bold special'>Our Services</h1>
+      </div>
+      <div class='service1 flex flex-col text-black justify-center items-center'>
+        <div class='svg w-96'>
+          <img src={service0} alt='Service' />
         </div>
-    )
+        <p class=' w-8/12 m-auto text-center font-semi-bold tracking-wider'>
+          At vero eos et accusamus et iusto odio dignissimos ducimus qui
+          blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
+          et quas molestias excepturi sint occaecati cupiditate non provident.
+        </p>
+      </div>
+      <div class='info w-9/12 mt-8 text-black m-auto'>
+        <div class='grid md:grid-cols-3 grid-cols-1 gap-3 justify-center items-center'>
+          <SingleService img={service1} text={p1} />
+          <SingleService img={service2} text={p1} />
+          <SingleService img={service3} text={p1} />
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Services;
+export default Services
--- src/components/saes/ApproachInfo.js
+++ src/components/saes/ApproachInfo.js
@@ -1,43 +1,39 @@
 const approach = [
-    {
-        id : 1,
-        count : '01',
-        title : 'Web Design',
-        text : 'Notre entreprise peut concevoir un tout nouveau site web. ou restructurer un site web existant.'
-    }, 
-    {
-    id : 2,
-    count : '02',
-    title : 'Web Site',
-    text : 'Un Site Web parfaitement conçu est le meilleur outil pour la publicité de la marque et entraine finalement un accroissement  des revenus.'
-    },
-    {
-        id : 3,
-        count : '03',
-        title : 'Business Types',
-        text : "Votre site web sera créé pour representer l’ identité de votre business."
-    }
-    
+  {
+    id: 1,
+    count: '01',
+    title: 'Web Design',
+    text: 'Notre entreprise peut concevoir un tout nouveau site web. ou restructurer un site web existant.',
+  },
+  {
+    id: 2,
+    count: '02',
+    title: 'Web Site',
+    text: 'Un Site Web parfaitement conçu est le meilleur outil pour la publicité de la marque et entraine finalement un accroissement  des revenus.',
+  },
+  {
+    id: 3,
+    count: '03',
+    title: 'Business Types',
+    text: 'Votre site web sera créé pour representer l’ identité de votre business.',
+  },
 ]
 
 const approach_info = [
-    {
-        id : 1,
-        count : '01',
-        title : 'Logo design',
-        text : 'Remember, logo design is an essential element to your general marketing strategy..'
-    },
-    {
-        id : 2,
-        count : '02',
-        title : 'Brand Design',
-        text : "Keeping the brand design meaningful helps in communicating the brain's timeless value effectively."
-    }
-
+  {
+    id: 1,
+    count: '01',
+    title: 'Logo design',
+    text: "N'oubliez pas que la conception de logo est un élément essentiel pour votre marketing générale.",
+  },
+  {
+    id: 2,
+    count: '02',
+    title: 'Brand Design',
+    text: "Garder le design de la marque significatif aide à communiquer efficacement l'identité de l'entreprise.",
+  },
 ]
 
+export default approach
 
-export default approach;
-
-export  {approach , approach_info};
-
+export { approach, approach_info }
